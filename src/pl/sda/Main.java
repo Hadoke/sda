7c992bbf7cd71a545c53b0ade482ab9e00488509
Łukasz Grabowski <lukaszgrabowski555@gmail.com>
2018-10-06 13:02:55 +0200

    add metod getMag()

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -3,7 +3,7 @@ package pl.sda;
 public class Main {
         //Miachał Babula
     public static void main(String ...args) {
-        System.out.println(getMag());
+        System.out.println(args.length + getMag());
     }
 
     private static String getMag() {
