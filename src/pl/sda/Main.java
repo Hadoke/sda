9c771591a9d0822ab272fcaf62aed533505d83f9
Łukasz Grabowski <lukaszgrabowski555@gmail.com>
2018-10-06 13:16:20 +0200

    add l1

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -3,7 +3,7 @@ package pl.sda;
 public class Main {
         //Miachał Babula
     public static void main(String ...args) {
-        System.out.println("l");
+        System.out.println("2l");
         System.out.println(getMag());
     }
 
